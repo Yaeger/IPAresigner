04ab14ee75914d26a31479384e1e39f3a2eb95d3
Aaron Yaeger <ayaeger@netspi-271-ay.localdomain>
2015-10-04 20:06:41 -0500

    Initial Commit

---
+++ resign/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  resign
+//
+//  Created by Aaron Yaeger on 10/4/15.
+//  Copyright © 2015 Aaron Yaeger. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
